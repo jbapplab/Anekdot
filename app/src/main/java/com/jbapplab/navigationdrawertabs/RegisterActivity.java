322da3c9f4649d984bda4ad1cfc3416d8b4fa47d
jbapplab <app.lab.jb@gmail.com>
2017-09-07 16:59:05 +0100

    Filex the uploading of registration
    NOT working username

--- app/src/main/java/com/jbapplab/navigationdrawertabs/RegisterActivity.java
+++ app/src/main/java/com/jbapplab/navigationdrawertabs/RegisterActivity.java
@@ -1,9 +1,19 @@
 package com.jbapplab.navigationdrawertabs;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -17,5 +27,66 @@ public class RegisterActivity extends AppCompatActivity {
         final EditText etUsername = (EditText) findViewById(R.id.etUsername);
         final EditText etPassword = (EditText) findViewById(R.id.etPassword);
         final Button bRegister = (Button) findViewById(R.id.bRegister);
+
+        /**
+         * When the user presses the register button we will get information from the fields pass it
+         * to com.jbapplab.navigationdrawertabs.RegisterRequest and then execute the request.
+         */
+        bRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //gets the text from the name field, converts it to a string and saves it to the name variable
+                final String name = etName.getText().toString();
+                final String username = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
+                //This gets the age. converts it to an int and saves it
+                final int age = Integer.parseInt(etAge.getText().toString());
+
+                Response.Listener<String> responseListener = new Response.Listener<String>(){
+
+                    @Override
+                    public void onResponse(String response) {
+                        /**
+                         * This happens when the response has been executed and volley here gives us the response from register.php
+                         * Now we need to convert this to a JSONObject because we encoded it like that in the PHP file.
+                         * It takes the response that volley has given us and converts it into a JSONObject
+                         */
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            //When we register we get a success response from the PHP file. We need to get that.
+                            boolean success = jsonResponse.getBoolean("success");
+
+
+                            if (success){
+                                //If successful it will take them to the login page
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                RegisterActivity.this.startActivity(intent);
+                            }else{
+                                //If not successful we want to display an error and user can select to retry
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                builder.setMessage("Registration Failed")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                //Now we have to create the request. It needs the 4 fields and a response listener
+                RegisterRequest registerRequest = new RegisterRequest(name, username, age, password, responseListener);
+                /**
+                 * Finally we need to add our request to a request queue. Then we ask to get the queue from volley, because
+                 * volley hold all of the requests
+                 */
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/jbapplab/navigationdrawertabs/RegisterRequest.java
@@ -0,0 +1,52 @@
+package com.jbapplab.navigationdrawertabs;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by JohnB on 07/09/2017.
+ */
+
+public class RegisterRequest extends StringRequest {
+    /**
+     * Allows us to make a request to the register.php file on the server
+     * and get a response as a string (extends StringRequest)
+     */
+
+    //Need to specify URL of the register.php
+    private static final String REGISTER_REQUEST_URL = "http://applabjb.000webhostapp.com/register.php";
+    //Need to create a map
+    private Map<String, String> params;
+
+    //Create a constructor for when the first instance of this class is created
+    public RegisterRequest(String name, String username, int age, String password, Response.Listener<String> listener){
+        /**
+         * We need to pass some data to volley which will allow it to execute our request
+         * -First thing we need to pass is the method - POST means we are gonna send some data to register.php and it
+         * will respond with some data.
+         * -Then we need to give volley the URL
+         * -Then we need to give it the listener: when volley has finished with the request it will inform the
+         * Response.Listener
+         * -TODO Finally we need to provide an error listener that volley will inform if something goes wrong with the request
+         */
+        super(Method.POST, REGISTER_REQUEST_URL, listener, null);
+
+        //We use the params for volley to pass the information to register.php
+        params = new HashMap<>();
+        //Now we need to put the data into the HashMap params.put("nameinPHP", nameinJAVA)
+        params.put("name",name);
+        params.put("username",username);
+        params.put("password",password);
+        //age+ '''' basically converts it from an integer to a string
+        params.put("age",age + "");
+    }
+    //Now that we have put the data in the params volley needs to access them
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
+
